e48b648d9837e8a1254522e6c9bd0fc7c8ad93eb
rasel3980 <rkrasel3980@gmail.com>
2026-02-08 09:45:04 +0600

    added some os function

---
+++ OS-module/index.js
@@ -0,0 +1,8 @@
+const os = require('os');
+
+console.log(os.userInfo());
+console.log(os.cpus());
+console.log(os.homedir());
+console.log(os.totalmem());
+console.log(os.freemem());
+console.log(os.version());
