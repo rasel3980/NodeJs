58a40804088a660f00cb3f5aa997a30508286daa
rasel3980 <rkrasel3980@gmail.com>
2026-02-20 15:52:06 +0600

    environment variable setup done

--- env-variable/index.js
+++ env-variable/index.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const hostName = process.env.BASE_URL;
+const PORT =process.env.PORT || 5000;
 app.get('/',(req,res)=>{
     res.send('Hello i am home route')
 });
 app.listen(PORT,()=>{
-    console.log(`server running at http://localhost:${PORT}`);
+    console.log(`server running at ${hostName}:${PORT}`);
 })
