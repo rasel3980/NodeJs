a4c6d8e636eb0bc8a825d34a934963a3bb9bcdb5
rasel3980 <rkrasel3980@gmail.com>
2026-02-20 17:14:47 +0600

    added middleware function

--- MiddleWare/index.js
+++ MiddleWare/index.js
@@ -2,11 +2,20 @@ const express = require('express');
 const PORT = 3000;
 const app = express();
 
+const middleWare = (req,res,next)=>{
+    console.log('My middleWare');
+    req.currentTime = new Date(Date.now())
+    next();
+}
 
-
-app.get('/',(req,res)=>{
+app.get('/',middleWare,(req,res)=>{
     res.send('<h1>Welcome to Home route</h1>')
+})
+app.get('/login',middleWare,(req,res)=>{
+    console.log('i am login route at.' + req.currentTime);
+    res.send('<h1>Welcome to Login route</h1>')
 })
 app.listen(PORT,()=>{
     console.log(`server running at http://localhost:${PORT}`);
+    console.log('hello');
 })
