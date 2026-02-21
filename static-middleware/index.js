1048094140edbcf1aed711c4bcb3206ffe98c63e
rasel3980 <rkrasel3980@gmail.com>
2026-02-21 22:57:38 +0600

    added html file

--- static-middleware/index.js
+++ static-middleware/index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/',(req,res)=>{
-    res.send('<h2>i am home route</h2>')
+    res.sendFile(__dirname + "/index.html")
 })
 app.listen(PORT,()=>{
     console.log(`server running at http://localhost:${PORT}`);
