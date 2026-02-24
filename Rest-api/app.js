275fe68ee18ef628a9c4827184ce8eb35e67f9ab
rasel3980 <rkrasel3980@gmail.com>
2026-02-24 15:14:13 +0600

    added 404 route not found

--- Rest-api/app.js
+++ Rest-api/app.js
@@ -8,4 +8,13 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + "/index.html")
+})
+
+// route not found
+
+app.use((req,res,next)=>{
+    res.status(404).json({"message":"Your request is not valid route"})
+})
 module.exports = app;
