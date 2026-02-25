8845c73337465ebd467d96223c831a6f360ddcc0
rasel3980 <rkrasel3980@gmail.com>
2026-02-25 23:53:12 +0600

    update user by put method

--- Rest-api/controllers/users.controllers.js
+++ Rest-api/controllers/users.controllers.js
@@ -1,9 +1,11 @@
 const users = require("../Models/user.model");
 const { v4: uuidv4 } = require('uuid');
 
+// get all user
 const getAllUser = (req,res)=>{
     res.status(200).json({users})
 }
+// create new user by post method
 const createUser = (req,res)=>{
     const newUser ={
         id:uuidv4(),
@@ -14,5 +16,11 @@ const createUser = (req,res)=>{
 
     res.status(200).json({users})
 }
+// update user by put method
+const updateUser = (req,res)=>{
+    const id = req.params.id
 
-module.exports = {getAllUser,createUser};
+    res.status(200).json({message: id})
+}
+
+module.exports = {getAllUser,createUser,updateUser};
--- Rest-api/routes/users.route.js
+++ Rest-api/routes/users.route.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { getAllUser ,createUser} = require('../controllers/users.controllers');
+const { getAllUser ,createUser,updateUser} = require('../controllers/users.controllers');
 const router = express.Router();
 router.get('/',getAllUser)
 router.post('/',createUser)
+router.put('/:id',updateUser)
 
 module.exports = router;
